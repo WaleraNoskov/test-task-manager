27eafba7a8ea4691b702d9c30c422d0c607e2193
Valery Noskov <rahdah06@gmail.com>
2025-07-07 09:11:54 +0300

    feat: basically added tasks-page and angular-material.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
+import {TasksPage} from './feature/tasks/pages/tasks-page/tasks-page';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: 'tasks', component: TasksPage, title: 'Tasks'},
+];
---
+++ src/app/feature/tasks/pages/tasks-page/tasks-page.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import {MatButton} from '@angular/material/button';
+
+@Component({
+  selector: 'app-tasks-page',
+  imports: [
+    MatButton
+  ],
+  templateUrl: './tasks-page.html',
+  styleUrl: './tasks-page.scss'
+})
+export class TasksPage {
+
+}
