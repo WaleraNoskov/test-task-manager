ea17883f1df260f18b666e78dfc3e051b822c8eb
Valery Noskov <rahdah06@gmail.com>
2025-07-07 12:48:04 +0300

    feat: added header to tasks.

--- src/app/feature/tasks/pages/tasks-page/tasks-page.ts
+++ src/app/feature/tasks/pages/tasks-page/tasks-page.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import {ManageTasksWidget} from '../../widgets/manage-tasks-widget/manage-tasks-widget';
+import {MatToolbar, MatToolbarRow} from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-tasks-page',
   imports: [
-    ManageTasksWidget
+    ManageTasksWidget,
+    MatToolbar,
+    MatToolbarRow
   ],
   templateUrl: './tasks-page.html',
   styleUrl: './tasks-page.scss'
